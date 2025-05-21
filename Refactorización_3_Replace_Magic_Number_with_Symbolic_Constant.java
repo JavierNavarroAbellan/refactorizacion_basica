46cd7da394ae526da46d250f72445340f3fef010
JavierNavarroAbellan <jnavarroabellan@gmail.com>
2025-05-21 13:25:22 +0200

    Refactorización 3 Replace Magic Number with Symbolic Constant

--- Refactorización_3_Replace_Magic_Number_with_Symbolic_Constant.java
+++ Refactorización_3_Replace_Magic_Number_with_Symbolic_Constant.java
@@ -1,10 +1,15 @@
-/*public class Refactorización {
+/*public class Refactorización_3_Replace_Magic_Number_with_Symbolic_Constant {
 
 private void totalMonedas() {
     int oro = 10;
     int plata = 3;
     int bronce = 7;
     int total = oro + plata + bronce;
+
+    if (total > 120){
+        
+        System.out.println("Peso máximo");
+    }
 
     if (total > 0){
     
@@ -21,3 +26,33 @@ private void totalMonedas() {
     }
 }
 }*/
+
+public class Refactorización_3_Replace_Magic_Number_with_Symbolic_Constant {
+
+private void totalMonedas() {
+    int oro = 10;
+    int plata = 3;
+    int bronce = 7;
+    int total = oro + plata + bronce;
+    int pesoMaximo = 120;
+    
+    if (total > pesoMaximo){
+        
+        System.out.println("Peso máximo");
+    }
+
+    if (total > 0){
+    
+    System.out.println("Monedas de oro: " + oro);
+    
+    System.out.println("Monedas de plata: " + plata);
+    
+    System.out.println("Monedas de bronce: " + bronce);
+    
+    System.out.println("Monedas totales: " + total);
+
+    }else{
+        System.out.println("No tienes dinero");
+    }
+}
+}
